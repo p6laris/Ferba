c127c9c90fc30add592b6ab0d4fd6222ba1af704
Yunis surchy <Yunissurchy@gmail.com>
2022-11-26 19:35:58 +0300

    Add project files.

---
+++ Ferba/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using Ferba.Tools;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ferba
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SoundManager _bgSound;
+        int _tabIndex = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Sound
+            _bgSound = new(Properties.Resources.Music, true);
+            _bgSound.PlaySound();
+        }
+
+        private void ClosePic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+
+        }
+
+        private void SoundPic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (_bgSound.IsPlaying)
+                {
+                    _bgSound.StopSound();
+                    SoundPic.Source = new BitmapImage(new Uri("./Assets/Images/SoundOn.png", UriKind.Relative));
+                }
+                else
+                {
+                    _bgSound.PlaySound();
+                    SoundPic.Source = new BitmapImage(new Uri("./Assets/Images/SoundOff.png", UriKind.Relative));
+                }
+            }
+        }
+
+
+        private void RightArrowPic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (ItemTabs.SelectedIndex <= ItemTabs.Items.Count)
+                    ++ItemTabs.SelectedIndex;
+
+            }
+
+        }
+
+        private void LeftArrowPic_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (ItemTabs.SelectedIndex >= 1)
+                    --ItemTabs.SelectedIndex;
+            }
+        }
+    }
+}
---
+++ Ferba/Tools/SoundManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Ferba.Tools
+{
+    internal class SoundManager : IDisposable
+    {
+        public bool IsPlaying { get; private set; }
+        SoundPlayer _player;
+        bool _isLooping;
+        private bool disposedValue;
+
+        public SoundManager(UnmanagedMemoryStream Stream, bool IsLooping = false)
+        {
+            _player = new(Stream);
+            IsLooping = _isLooping;
+        }
+
+        /// <summary>
+        /// Play's the sound.
+        /// </summary>
+        public void PlaySound()
+        {
+            if (_isLooping)
+                _player.PlayLooping();
+            else
+                _player.Play();
+
+            IsPlaying = true;
+        }
+
+        /// <summary>
+        /// Stop's the sound.
+        /// </summary>
+        public void StopSound()
+        {
+            if (IsPlaying)
+                _player.Stop();
+
+            IsPlaying = false;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _player.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        { 
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
